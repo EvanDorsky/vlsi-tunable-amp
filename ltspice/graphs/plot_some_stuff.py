8cd88646de2a765b23107dfdb8f90a7c072c2192
Forrest Bourke <forrestbourke@gmail.com>
2014-12-04 15:02:10 -0500

    added a figure showing the logic working

--- ltspice/graphs/plot_some_stuff.py
+++ ltspice/graphs/plot_some_stuff.py
@@ -5,34 +5,29 @@ import csv, numpy
 rc('text', usetex=True)
 rc('font',**{'family':'serif','serif':['Computer Modern Roman']})
 
-with open('gaindata.txt', newline='') as inputfile:
+with open('spi-b0b1csnmosi.txt', newline='') as inputfile:
 	results = list(csv.reader(inputfile))
 
-plotme = numpy.zeros((3,len(results)-1))
+plotme = numpy.zeros((5,len(results)-1))
 
 i = 0
 for x in results[1:len(results)]:
 	plotme[0][i] = x[0].split("\t")[0]
 	plotme[1][i] = x[0].split('\t')[1]
 	plotme[2][i] = x[0].split('\t')[2]
+	plotme[3][i] = x[0].split('\t')[3]
+	plotme[4][i] = x[0].split('\t')[4]
 	i += 1
 
-#add divisions where the gain changes
-plt.plot((-10,320), (3.5-.10, 3.5-.10),color=[.5,.5,.5])
-plt.plot((-10,320), (3.5+.15, 3.5+.15),color=[.5,.5,.5])
-plt.text(325,3.5,r'\textbf{Gain}',fontsize=12,verticalalignment='center')
-for i in range(16):
-	plt.plot((20*i, 20*i), (0, 5), '--', color=[.7,.7,.7])
-	if i == 0:
-		plt.text(20*i+10,3.5,"$\infty$",horizontalalignment='center')#,color='grey')#"∞")
-	else:
-		plt.text(20*i+10,3.5,str(round((16/i + 1),1)), fontsize=8,horizontalalignment='center')#,color='grey')
 
-plt.plot(plotme[0]*1e6,plotme[1],'r-', linewidth='2', label="Input")
-plt.plot(plotme[0]*1e6,plotme[2],'b-', linewidth='2', label="Output")
-plt.xlim((0,3200))
+
+plt.plot(plotme[0]*1e6,plotme[1],'r-', linewidth='2', label="B0")
+plt.plot(plotme[0]*1e6,plotme[2]+6,'b-', linewidth='2', label="B1")
+plt.plot(plotme[0]*1e6,plotme[3]+18,'g-', linewidth='2', label="CSn")
+plt.plot(plotme[0]*1e6,plotme[4]+12,'m-', linewidth='2', label="MOSI")
+plt.xlim((0,3000))
 
 plt.legend()
 plt.xlabel("Time ($\mu{}S$)")#μS)")
-plt.ylabel("Voltage")
+
 plt.show()
